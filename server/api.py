05402fe60da249dd41818fceb7b2e79de367c6ea
chad <chadshankweiler@gmail.com>
2025-06-25 13:37:16 -0400

    added: save json data to database

--- server/api.py
+++ server/api.py
@@ -26,6 +26,9 @@ def add_guide():
     for a in articles: 
         row = {
             "id" : a.get("id"),
+            "county" : meta.get("county"),
+            "state" : meta.get("state"),
+            "country" : meta.get("country"),
             "title" : a.get("title"),
             "published_utc" : a.get("published_utc"),
             "source" : a.get("source"),
@@ -40,34 +43,64 @@ def add_guide():
 
 @app.route('/huh', methods=["POST"])
 def test():
+
+    conn = psycopg2.connect("dbname=postgres user=postgres password=lol host=localhost port=5432")
+
+    cur = conn.cursor()
+
+    # regex to remove markdown from json data
     pattern = re.compile(r"```json\s*(\{[\s\S]*?\})\s*```", re.DOTALL)
 
     rows = []
 
+    # raw data
     raw = request.get_json(force=True, silent=True)
     if not raw or "data" not in raw:
         return jsonify(error="JSON must contain 'name'"), 400
 
     markdown_block = raw["data"]
 
+    # remove markdown from text
     match = pattern.search(markdown_block)
 
+    # convert to json
     payload = json.loads(match.group(1))
 
-    articles = payload["articles"]
+    # append to rows array
+    meta = payload["query"]
+    for article in payload["articles"]: 
+        sha = article["id"] or canonical_sha(article["title"], article["summary"])
+        rows.append((
+            sha,
+            meta["county"],
+            meta["state"],
+            meta["country"],
+            article["title"],
+            article["published_utc"],
+            article["source"],
+            article["url"],
+            article.get("summary"),
+            json.dumps(article, ensure_ascii=False)
+        ))
 
-    for a in articles: 
-        row = {
-            "id" : a.get("id"),
-            "title" : a.get("title"),
-            "published_utc" : a.get("published_utc"),
-            "source" : a.get("source"),
-            "url" : a.get("url"),
-            "summary" : a.get("summary"),
-        }
-        rows.append(row)
 
-    print(rows)
+    insert_sql = """
+        INSERT INTO news_articles
+          (id, county, state, country, title, published_utc,
+           source, url, summary, raw)
+        VALUES %s
+        ON CONFLICT (id) DO NOTHING;
+    """
+
+    execute_values(cur, insert_sql, rows, page_size=500)
+    conn.commit()
+
+    cur.execute("SELECT COUNT(*) FROM news_articles;")
+    print(cur.fetchone()[0], "rows now in table")
+
+
+    cur.close()
+    conn.close()
 
     return jsonify(payload), 201   
 
